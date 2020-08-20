e3ff87238f0973c2a5d62bd96883a8068a830d2d
sudong0701 <46431309+sudong0701@users.noreply.github.com>
2020-08-20 10:37:27 +0800

    work语法修改为ts

--- src/page/js/work/work.ts
+++ src/page/js/work/work.ts
@@ -1,52 +1,63 @@
-/// <reference path="../../../index.d.ts" />
+/// <reference path="../../../typing/index.d.ts" />   
 
 import '@/page/css/work/work.scss';
-const { axios } = require('@/utils/axios')
 
-
-$(function() {
-    CommonUtils.setHtmlTitle('操作指南')
-    initData()
-    $('#actionTab').click(function () {
-        changeTab('action')
-    })
-    $('#questionTab').click(function () {
-        changeTab('question')
-    })
-    $('#infoTab').click(function () {
-        changeTab('info')
-    })
-})
-
-
-/**
- 初始化数据
- @param
- @return
- */
-function initData():void {
-    $('#actionContent>p').text('这是操作流程页面')
-    $('#questionContent>p').text('这是常见问题页面')
-    $('#infoContent>p').text('这是方案说明页面')
-}
-
-/**
- 切换tab页
- @param {string} type tab页类型
- @return
- */
-function changeTab(type: string): void {
-    $('#tab').children().removeClass('active')
-    $('.content').removeClass('active')
-    if (type == 'action') {
-        $('#actionTab').addClass('active')
-        $('#actionContent').addClass('active')
-    } else if (type == 'question') {
-        $('#questionTab').addClass('active')
-        $('#questionContent').addClass('active')
-    } else if (type == 'info') {
-        $('#infoTab').addClass('active')
-        $('#infoContent').addClass('active')
+declare global {
+    interface Window { 
+        axios: any; 
+        Router: any
     }
 }
+
+class work {
+    constructor() {
+        this.initData()
+    }
+    /**
+     初始化数据
+     @param
+     @return
+    */
+    initData(): void {
+        window.axios.post()    //接口调用示例
+        $('#actionContent>p').text('这是操作流程页面')
+        $('#questionContent>p').text('这是常见问题页面')
+        $('#infoContent>p').text('这是方案说明页面')
+        $('#actionTab').click(() => {
+            this.changeTab('action')
+        })
+        $('#questionTab').click(() => {
+            this.changeTab('question')
+        })
+        $('#infoTab').click(() => {
+            this.changeTab('info')
+            window.Router.push('main/main')
+        })
+        console.log(window.Router)
+    }
+    /**
+     切换tab页
+     @param {string} type tab页类型
+     @return
+    */
+    changeTab(type: string): void {
+        $('#tab').children().removeClass('active')
+        $('.content').removeClass('active')
+        if (type == 'action') {
+            $('#actionTab').addClass('active')
+            $('#actionContent').addClass('active')
+        } else if (type == 'question') {
+            $('#questionTab').addClass('active')
+            $('#questionContent').addClass('active')
+        } else if (type == 'info') {
+            $('#infoTab').addClass('active')
+            $('#infoContent').addClass('active')
+        }
+    }
+}
+
+$(function () {
+    CommonUtils.setHtmlTitle('操作指南')
+    new work()
+})
 
