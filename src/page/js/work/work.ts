b86fae74d25b5f6d1fc6c30a2fd82bd831bea3e2
sudong.duan <1006364428@qq.com>
2021-05-06 14:42:17 +0800

    <feat>[work/work.ts]:冗余代码删除

--- src/page/js/work/work.ts
+++ src/page/js/work/work.ts
@@ -11,66 +11,34 @@ declare global {
     }
 }
 
-class work {
-    constructor() {
-        this.initData()
-    }
-    /**
-     初始化数据
-     @param
-     @return
-    */
-    initData(): void {
-        window.axios.post()    //接口调用示例
-        $('#actionContent>p').text('这是操作流程页面')
-        $('#questionContent>p').text('这是常见问题页面')
-        $('#infoContent>p').text('这是方案说明页面')
-        $('#actionTab').click(() => {
-            this.changeTab('action')
-        })
-        $('#questionTab').click(() => {
-            this.changeTab('question')
-        })
-        $('#infoTab').click(() => {
-            this.changeTab('info')
-            window.Router.push('main/main')
-        })
-    }
-    /**
-     切换tab页
-     @param {string} type tab页类型
-     @return
-    */
-    changeTab(type: string): void {
-        $('#tab').children().removeClass('active')
-        $('.content').removeClass('active')
-        if (type == 'action') {
-            $('#actionTab').addClass('active')
-            $('#actionContent').addClass('active')
-        } else if (type == 'question') {
-            $('#questionTab').addClass('active')
-            $('#questionContent').addClass('active')
-        } else if (type == 'info') {
-            $('#infoTab').addClass('active')
-            $('#infoContent').addClass('active')
-        }
-    }
-}
 
 $(function () {
     CommonUtils.setHtmlTitle('操作指南')
-    new work()
     new Vue({
         el: 'app',
         data: {
             text: 'hello world',
             className: 'green',
-            bgColor: '#5a96ec'
+            bgColor: '#5a96ec',
+            textColor: '#e99d3a',
+            isChange: false,
+            isShow: false
         },
         methods: {
-            reset() {
-                this.className = 'red'
-                this.bgColor = '#67c23a'
+            changeColor() {
+                if(!this.isChange) {
+                    this.className = 'red'
+                    this.bgColor = '#67c23a'
+                    this.textColor = '#fff'
+                    this.isShow = true
+                    this.isChange = true
+                } else {
+                    this.className = 'green'
+                    this.bgColor = '#5a96ec'
+                    this.textColor = '#e99d3a'
+                    this.isShow = false
+                    this.isChange = false
+                }
             }
         }
     })
